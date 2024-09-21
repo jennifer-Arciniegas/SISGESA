e14af9722843578a63595ce571bf8f8abe3dbe08
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2024-09-21 00:39:20 -0500

    interfaces principales V1

---
+++ interfaz/menu.py
@@ -0,0 +1,23 @@
+def menu():
+    while True:
+        print("** MENU**")
+        print("a. Registro de grupos.")
+        print("b. Registro de módulos.")
+        print("c. Registro de estudiantes.")
+        print("d. Registro de docentes.")
+        print("e. Registro de asistencia.")
+        print("f. Consultas de información.")
+        print("g. Generación de informes.")
+        print("h. Cambio de contraseña.")
+        print("i. Salida del sistema.")
+
+        print(">> Ingresar a opcion: ", end="")
+        
+        try:
+            opcion = input(lower())
+            if 
+            pass
+        except ValueError:
+            print("Error opcion invalida")
+            print("ingrese cualquier tecla para volver al menu")
+            
