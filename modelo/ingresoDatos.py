95517869e1d52f64734658426dd5e20c4d6edbff
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2024-09-21 04:32:32 -0500

    v4 funciones de grupo

--- modelo/ingresoDatos.py
+++ modelo/ingresoDatos.py
@@ -1,2 +1,60 @@
+#se presentan las fuciones que permitiran almacenar el dicionario de grupos.
+
+def leerSgla():
+    while True: #validar que se ha ingresado un valor 
+        try:
+            sigla = int(input("Codigo del grupo: \n"))
+            if len(sigla.strip()) == 0: 
+                print("Error. Codigo invalido")
+                continue
+            return sigla
+        except Exception: #valida cualquier error
+            print("Error al ingresar el codigo")
+
+
+def leerNombre():
+     while True: #validar que se ha ingresado un valor 
+        try:
+            name = int(input("Codigo del grupo: \n"))
+            if len(name.strip()) == 0: 
+                print("Error. Codigo invalido")
+                continue
+            return name 
+        except Exception: #valida cualquier error
+            print("Error al ingresar el codigo")
+
+def leercodigo():
+    while True: #validar que se ha ingresado un valor 
+        try:
+            cod = int(input("Codigo del grupo: \n"))
+            if len(cod.strip()) == 0: 
+                print("Error. Codigo invalido")
+                continue
+            return cod 
+        except Exception: #valida cualquier error
+            print("Error al ingresar el codigo")
+
+
+
 def registroGrupos():
-    pass
+    print("Registrar Grupo")
+    cod = leercodigo()
+    if cod not in grup: # si el codigo no esta en la libreria entonces lo guardara, si esta enviara al else
+        nombre = leerNombre()
+        sigla = leerSgla()
+
+        datgrup = {
+            "nombre": nombre,
+            "sigla": sigla
+        }
+
+        grup[cod] = datgrup   #cod es el codigo y en datagrup estan los valores de esa llave 
+        grup = dict(sorted(grup.items())) #ordena los valores ingresados 
+
+        guardar(grup)
+    else:
+        print("El codigo ya esta registrado, por lo tanto el grupo ya esta registrado")
+        print("ingrese cualquier tecla para volver al menu. \n")
+
+    return grup 
+
