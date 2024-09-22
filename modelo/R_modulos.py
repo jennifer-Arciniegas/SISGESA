a373c6f26c1dbffa4b6b5974dd586af7be1a3b4b
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2024-09-22 17:11:20 -0500

    v12

--- modelo/R_modulos.py
+++ modelo/R_modulos.py
@@ -14,3 +14,54 @@ modulo = {
 }
 """
 
+from SISGESA.persistencia.pesistenciaGuardar import guardar
+
+
+def R_modulos (registro, arch):
+    print("\n\n ** Registrar Estudiante **")
+    cod = leerCodigo()
+    if cod not in registro:
+        nombre = leernombre()
+        D_semanas = Duracion()
+
+        datModulo={
+            "Nombre": nombre,
+            "Duracion": D_semanas
+
+        }
+        registro[cod]= datModulo
+        registro = dict(sorted(registro.items()))
+        guardar(registro)
+
+
+    else:
+        
+
+    
+
+
+
+
+
+def Re_estudiantes(registro, arch):
+    print("\n\n ** Registrar Estudiante **")
+    cod = leerCodigo()
+    if cod not in registro:
+        nombre = leerNombre()
+        sexo = leerSexo()
+        edad = leerEdad()
+
+        datEstudiante={
+            "Nombre": nombre,
+            "Sexo": sexo,
+            "Edad": edad
+        }
+
+        registro[cod] =datEstudiante
+        registro = dict(sorted(registro.items()))
+        guardar(registro)
+    else:
+        print("El estudiante ya esta registrado")
+
+    input("Precione cualquier tecla para volver al menu. \n")
+    guardar(registro)
