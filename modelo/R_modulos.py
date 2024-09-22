e5e2bdb62b06a9c20502cc9822d3eca0a5452d4c
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2024-09-22 03:09:09 -0500

    v11

---
+++ modelo/R_modulos.py
@@ -0,0 +1,16 @@
+
+""" estructura
+modulo = {
+    codigo1(str):{
+        nombre:(str)
+        duracion: (int)
+        
+    },
+    codigo2(str):{
+       nombre:(str)
+       duracion: (int)
+    },
+
+}
+"""
+
