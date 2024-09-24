358bddc6e3e859fe69eb47ea197ac123486ccc93
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2024-09-24 08:17:34 -0500

    v29

--- index.py
+++ index.py
@@ -6,28 +6,28 @@ from modelo.R_Modulo import registroModulo
 from persistencia.cargar import *
 from modelo.R_Docentes import registroDocentes
 from modelo.iniciosesion import login
+from modelo.iniciosesion import cambiarpassword
 
-
-login()
-while True:
-    opc = menu()
-    match opc:
-        case "a":
-            guardar(registroGrupos(grupos),"grupos") 
-        case "b":
-            guardar(registroModulo(modulo), "Modulo")
-        case "c":
-            guardar(Re_estudiantes(estudiantes),"Estudiantes")
-        case "d":
-            guardar(registroDocentes(docentes), "Docentes")
-        case "e":
-            asistencia = registroAsistencia(asistencia, archivo)
-        case "f":
-            consultaIfo = consultaInfor(consultaIfo, archivo)
-        case "g":
-            Ginformes = genInformes(Ginformes, archivo)
-        case "h":
-            cambio_clave = cambioClave(cambio_clave, archivo)
-        case "i":
-            print("Gracias por usar el software")
-            break 
+if login() is not None:
+    while True:
+        opc = menu()
+        match opc:
+            case "a":
+                guardar(registroGrupos(grupos),"grupos") 
+            case "b":
+                guardar(registroModulo(modulo), "Modulo")
+            case "c":
+                guardar(Re_estudiantes(estudiantes),"Estudiantes")
+            case "d":
+                guardar(registroDocentes(docentes), "Docentes")
+            case "e":
+                asistencia = registroAsistencia(asistencia, archivo)
+            case "f":
+                consultaIfo = consultaInfor(consultaIfo, archivo)
+            case "g":
+                Ginformes = genInformes(Ginformes, archivo)
+            case "h":
+                guardar(cambiarpassword(cuenta), "cuenta")
+            case "i":
+                print("Gracias por usar el software")
+                break 
